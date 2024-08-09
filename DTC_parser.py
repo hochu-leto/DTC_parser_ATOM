14eaadb542e20298a4d83996aa8a40dac76703d3
hochu-leto <hochu-leto@yandex.ru>
2024-08-09 14:50:01 +0300

    Made common english-china separator for dtc desc

--- DTC_parser.py
+++ DTC_parser.py
@@ -10,7 +10,7 @@ word_in_column_name_for_delete = 'Self-healing'
 language_to_translate = 'Russian'
 translated_file_name = 'DTC.xlsx'
 dict_of_languages = dict(Russian='ru', English='en', Chinese='zh-TW')
-list_for_translated_dtc = list()
+russian_dtc_list = list()
 list_for_translated_repair = list()
 list_for_english_repair = list()
 language = 'English'
@@ -21,7 +21,7 @@ dtc_char_to_byte_dict = dict(
     U=0b11
 )
 dtc_layout_headers_list_to_df = ['DTC Display', 'DTC Description', 'Repair action']
-ibs_english_dtc_list = list()
+english_dtc_list = list()
 ibs_chinese_dtc_list = list()
 cjk_ranges = [
     (0x4E00, 0x62FF),
@@ -51,11 +51,13 @@ def is_cjk(char):
             return True
     return False
 
+
 def is_str_cjk(st: str) -> bool:
     for ch in st:
         if is_cjk(ch):
             return True
     return False
+
 
 def get_df_from_questionary(file_locate: str) -> pd.DataFrame:
     if DTC in pd.ExcelFile(file_locate).sheet_names:
@@ -64,14 +66,14 @@ def get_df_from_questionary(file_locate: str) -> pd.DataFrame:
             if dtc[0] == column_name_for_translate:
                 list_dtc_for_translate = dtc[1]
                 language = list_dtc_for_translate[0]
-                list_for_translated_dtc.append(language_to_translate)
+                russian_dtc_list.append(language_to_translate)
                 for dtc_for_translate in list_dtc_for_translate[1:]:
-                    list_for_translated_dtc.append(GoogleTranslator(source=dict_of_languages[language],
-                                                                    target=dict_of_languages[language_to_translate]
-                                                                    ).translate(dtc_for_translate))
+                    russian_dtc_list.append(GoogleTranslator(source=dict_of_languages[language],
+                                                             target=dict_of_languages[language_to_translate]
+                                                             ).translate(dtc_for_translate))
             elif word_in_column_name_for_delete in dtc[0]:
                 del df_dtc_sheet[dtc[0]]
-        df_dtc_sheet[language_to_translate] = list_for_translated_dtc
+        df_dtc_sheet[language_to_translate] = russian_dtc_list
         df_dtc_sheet.columns = df_dtc_sheet.iloc[0]
         df_dtc_sheet = df_dtc_sheet.iloc[1:, :]
         try:
@@ -96,20 +98,20 @@ def get_df_from_dtc_layout(file_locate: str) -> pd.DataFrame:
                 list_dtc_for_translate = dtc[1]
 
                 for dtc_for_translate in list_dtc_for_translate:
-                    # only for IBS
-                    ibs_lang_list = list(filter(None, dtc_for_translate.split('\n')))
-                    ibs_dtc_chinese_description = ibs_lang_list[1]
-                    ibs_dtc_english_description = ibs_lang_list[2] if len(ibs_lang_list) >= 3 else \
+                    dtc_eng_list = [i for i in dtc_for_translate.split() if is_str_cjk(i)]
+                    dtc_cjk_list = [i for i in dtc_for_translate.split() if not is_str_cjk(i)]
+
+                    dtc_english_description = ' '.join(dtc_eng_list) if dtc_eng_list else ' '.join([
                         GoogleTranslator(source=dict_of_languages['Chinese'],
-                                         target=dict_of_languages[language]).translate(ibs_dtc_chinese_description)
-                    ibs_english_dtc_list.append(ibs_dtc_english_description)
-                    ibs_chinese_dtc_list.append(ibs_dtc_chinese_description)
-                    list_for_translated_dtc.append(GoogleTranslator(source=dict_of_languages[language],
-                                                                    target=dict_of_languages[language_to_translate]
-                                                                    ).translate(ibs_dtc_english_description))
-                df_dtc_sheet[language_to_translate] = list_for_translated_dtc
-                df_dtc_sheet['English'] = ibs_english_dtc_list
-                df_dtc_sheet['Chinese'] = ibs_chinese_dtc_list
+                                         target=dict_of_languages[language]).translate(i) for i in dtc_cjk_list])
+
+                    english_dtc_list.append(dtc_english_description)
+                    russian_dtc_list.append(GoogleTranslator(source=dict_of_languages[language],
+                                                             target=dict_of_languages[language_to_translate]
+                                                             ).translate(dtc_english_description))
+                df_dtc_sheet[language_to_translate] = russian_dtc_list
+                df_dtc_sheet['English'] = english_dtc_list
+                # df_dtc_sheet['Chinese'] = ibs_chinese_dtc_list
             elif dtc_layout_headers_list_to_df[0] in dtc[0]:
                 # это, конечно, порнуха
                 df_dtc_sheet['3-Bytes DTC'] = dtc[1]
@@ -118,8 +120,8 @@ def get_df_from_dtc_layout(file_locate: str) -> pd.DataFrame:
                 list_repair_for_translate = dtc[1]
                 for repair_for_translate in list_repair_for_translate:
                     english_sentence = [i for i in repair_for_translate.split() if
-                                     not is_str_cjk(i)]
-                    english_desc = ' '.join(english_sentence)
+                                        not is_str_cjk(i)]
+                    english_desc = '\n'.join(english_sentence)
                     list_for_english_repair.append(english_desc)
                     list_for_translated_repair.append(GoogleTranslator(source=dict_of_languages[language],
                                                                        target=dict_of_languages[language_to_translate]
